58cbe29e4385924e3aff6f42009438be6f67ca48
haddadiamirhosein <haddadiamirhosein.d@gmail.com>
2022-07-20 10:29:32 +0430

    in this part user can add some post and making change in style of weblog

--- articles/views.py
+++ articles/views.py
@@ -9,17 +9,24 @@ def articles_list(request):
     args = {'article':articles}
     return render(request , 'articles/articleslist.html', args)
 
+
+
 def articles_detile(request , article_slug):
     # return HttpResponse(article_slug)
     article = models.Article.objects.get(slug = article_slug)
     return render(request , 'articles/articles_detile.html',{'article_detile':article})
 
+
+
 @login_required(login_url = "/accounts/login")
 def articles_create(request):
     if request.method == 'POST':
         form = forms.CreateArticle(request.POST,request.FILES)
-        if form.is_valid():
-            # save
+        if form.is_valid:
+            instance = form.save(commit = False)
+            instance.author = request.user
+            instance.save()
             return redirect('articles_urls:list')
-    form = forms.CreateArticle()
+    else:
+        form = forms.CreateArticle()
     return render(request , 'articles/articles_create.html',{'form':form})
